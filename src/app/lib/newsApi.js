0b59b6380c389f92cb37f37effafda58bd67331d
Biruk Dejene <birukedjn@gmail.com>
2025-10-06 10:45:23 +0300

    reinitialized

--- src/app/lib/newsApi.js
+++
@@ -1,7 +0,0 @@
-export async function getNews() {
-  const res = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=eee13f24a2f8496dbe681e555ab69833`
-  );
-  const data = await res.json();
-  return data.articles;
-}
